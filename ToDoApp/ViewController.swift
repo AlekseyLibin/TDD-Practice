b894f07adbc42ac4994cb03a99c5c5db6be5e58c
Aleksey Libin <lexa.libin@gmail.com>
2023-01-29 13:33:17 +0100

    Initial commit

---
+++ ToDoApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ToDoApp
+//
+//  Created by Aleksey Libin on 29.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
